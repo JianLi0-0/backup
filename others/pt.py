b8913d36dff985160753846add976de12d41b2e3
Li Jian <desmondgglee@gmail.com>
2021-01-03 21:35:08 +0800

    add calibration package; change moveit controller; calibrate for gazebo ft sensor

--- others/pt.py
+++ others/pt.py
@@ -3,19 +3,20 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 pub = rospy.Publisher('/pos_joint_traj_controller/command', JointTrajectory, queue_size=1)
 rospy.init_node('talker', anonymous=True)
-rate = rospy.Rate(0.2) # 10hz
+rate = rospy.Rate(1) # 10hz
 # while not rospy.is_shutdown():
 p1 = JointTrajectoryPoint()
 p2 = JointTrajectoryPoint()
 p1.positions=[0,0,0,0,0,0]
 p1.time_from_start.secs = 2
 p1.velocities=[1.0,-1.0,1.0,1.0,1.0,1.0]
-p2.positions=[0,0,0,0,0,0]
+p2.positions=[-0.268793, -1.82578, 1.84404, -2.05047, 0.0987335, -0.408469]
 p2.time_from_start.secs = 5
 p2.velocities=[1.0,-1.0,1.0,1.0,1.0,1.0]
 t = JointTrajectory()
 t.joint_names = ["shoulder_pan_joint","shoulder_lift_joint", "elbow_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"]
 t.points.append(p1)
 t.points.append(p2)
+rate.sleep()
 pub.publish(t)
 rate.sleep()
--- ur5e_sim_force.sh
+++ ur5e_sim_force.sh
@@ -3,3 +3,4 @@ roslaunch industry_library_robot camera_calibration_pub.launch&
 sleep 1;roslaunch ur_e_gazebo ur5e.launch transmission_hw_interface:=hardware_interface/PositionJointInterface&# transmission_hw_interfac:=hardware_interface/VelocityJointInterface&
 # sleep 3;roslaunch ur5_e_moveit_config ur5_e_moveit_planning_execution.launch sim:=true&
 # sleep 2;roslaunch ur5_e_moveit_config moveit_rviz.launch config:=true;
+sleep 4;roslaunch gravity_compensation gravity_compensation_sim.launch;
