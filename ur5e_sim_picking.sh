c7cfb304e3d2f931ff9df962b04886ae5adf5287
Li Jian <desmondgglee@gmail.com>
2021-01-16 19:31:54 +0800

    replace rs_camera.launch with rs_rgbd.launch file to fix unorganized pointcloud problem

--- ur5e_sim_picking.sh
+++ ur5e_sim_picking.sh
@@ -5,6 +5,6 @@ sleep 2;
 roslaunch ur5_e_moveit_config ur5_e_moveit_planning_execution.launch sim:=true&
 sleep 1;
 roslaunch ur5_e_moveit_config moveit_rviz.launch config:=true&
-roslaunch realsense2_camera rs_camera.launch&
+roslaunch realsense2_camera rs_rgbd.launch&
 sleep 1;
 roslaunch industry_library_robot camera_calibration_pub.launch;
